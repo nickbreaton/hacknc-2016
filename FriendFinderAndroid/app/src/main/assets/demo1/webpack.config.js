c5763f987f667730f13f02d7b88e4773d0613c68
Nick Breaton <nick@breaton.com>
2016-10-29 16:37:23 -0400

    Reorganize

--- FriendFinderAndroid/app/src/main/assets/demo1/webpack.config.js
+++ FriendFinderAndroid/app/src/main/assets/demo1/webpack.config.js
@@ -13,8 +13,5 @@ module.exports = {
       { test: /\.css$/, loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]' }
     ]
   },
-  devtool: 'eval',
-  devServer: {
-    contentBase: './public'
-  }
+  devtool: 'eval'
 }
