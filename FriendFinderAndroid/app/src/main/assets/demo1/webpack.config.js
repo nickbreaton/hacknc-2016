2cf59ae82f7958bd29a3595b9c7627a3798791df
Nick Breaton <nick@breaton.com>
2016-10-29 16:33:04 -0400

    Convert assets to webpack

---
+++ FriendFinderAndroid/app/src/main/assets/demo1/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  context: __dirname,
+  entry: './src/index.js',
+  output: {
+    path: 'dist',
+    filename: 'index.js',
+    publicPath: 'dist'
+  },
+  module: {
+    loaders: [
+      { test: /\.json$/, loader: 'json' },
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
+      { test: /\.css$/, loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]' }
+    ]
+  },
+  devtool: 'eval',
+  devServer: {
+    contentBase: './public'
+  }
+}
