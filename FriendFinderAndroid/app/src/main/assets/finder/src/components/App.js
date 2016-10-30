3213d3ad750b617e0c727fba239fb4ef5be59fa9
Nick Breaton <nick@breaton.com>
2016-10-29 23:21:39 -0400

    Add quick changing

--- FriendFinderAndroid/app/src/main/assets/finder/src/components/App.js
+++ FriendFinderAndroid/app/src/main/assets/finder/src/components/App.js
@@ -9,17 +9,23 @@ export default class App extends React.Component {
   constructor(props) {
     super(props)
     this.state ={
-      current: 0
+      index: 1
     }
+  }
+
+  setIndex = index => {
+    this.setState({
+      index
+    })
   }
 
   render() {
     return (
       <div>
-        {/* <Map/> */}
-        <Camera/>
-        {/* <FriendsList/> */}
-        <BottomBar/>
+        { this.state.index == 0 ? <Map/> : null }
+        { this.state.index == 1 ? <Camera/> : null }
+        { this.state.index == 2 ? <FriendsList/> : null }
+        <BottomBar index={this.state.index} setIndex={this.setIndex}/>
       </div>
     )
   }
--- FriendFinderAndroid/app/src/main/assets/finder/src/components/BottomBar.js
+++ FriendFinderAndroid/app/src/main/assets/finder/src/components/BottomBar.js
@@ -5,20 +5,23 @@ import styles from '../styles/BottomBar.css'
 
 export default (props) => (
   <div className={styles.main}>
-    <BottomNavigation selectedIndex={1}>
+    <BottomNavigation selectedIndex={props.index}>
       <BottomNavigationItem
         label="Map"
         icon={<FontIcon className="material-icons">map</FontIcon>}
+        onTouchTap={() => props.setIndex(0)}
       />
 
       <BottomNavigationItem
         label="Camera"
         icon={<FontIcon className="material-icons">camera</FontIcon>}
+        onTouchTap={() => props.setIndex(1)}
       />
 
       <BottomNavigationItem
         label="Friends"
         icon={<FontIcon className="material-icons">people</FontIcon>}
+        onTouchTap={() => props.setIndex(2)}
       />
 
     </BottomNavigation>
