b5b6c12c0398f9c6ae6b9d885e02847acebcbca1
iganbold <itgel.ganbold1@gmail.com>
2016-10-29 23:51:45 -0400

    Stop create model based on user location

--- FriendFinderAndroid/app/src/main/assets/finder/lib/location.js
+++ FriendFinderAndroid/app/src/main/assets/finder/lib/location.js
@@ -1,17 +1,62 @@
-function locationChanged(lat, lon, alt, acc) {
-  var marker_image = new AR.ImageResource("assets/pin.png");
-  var marker_loc = new AR.GeoLocation(35.909896, -79.046767, alt);
-  var marker_drawable = new AR.ImageDrawable(marker_image, 8);
-  var marker_object = new AR.GeoObject(marker_loc, {
-    drawables: {
-      cam: [marker_drawable]
+//function locationChanged(lat, lon, alt, acc) {
+//  var marker_image = new AR.ImageResource("assets/pin.png");
+//  var marker_loc = new AR.GeoLocation(35.909896, -79.046767, alt);
+//  var marker_drawable = new AR.ImageDrawable(marker_image, 8);
+//  var marker_object = new AR.GeoObject(marker_loc, {
+//    drawables: {
+//      cam: [marker_drawable]
+//    }
+//  });
+//
+//  AR.logger.debug("lat:"+lat+", long: "+lon);
+//
+//}
+//
+//AR.logger.activateDebugMode();
+//
+//AR.context.onLocationChanged = locationChanged;
+
+
+var World = {
+    init: function initFn() {
+
+            AR.logger.activateDebugMode();
+    		AR.context.onLocationChanged = function locationChanged(lat, lon, alt, acc) {
+
+    		    World.myLocation = {"latitude": lat, "longitude" : lon, "altitude" : alt };
+    		    AR.logger.debug("lat: "+lat+", lon: "+lon);
+
+//    		    World.createModelAtLocation(World.myLocation);
+
+//    		    if (!World.created) {
+//                    World.created = true;
+//                    World.createModelAtLocation(World.myLocation);
+//                }
+            }
+    	},
+
+    createModelAtLocation: function createModelAtLocationFn(location) {
+
+        var marker_image = new AR.ImageResource("assets/pin.png");
+        var marker_loc = new AR.GeoLocation(location.latitude - 0.005, location.longitude + 0.0005, AR.CONST.UNKNOWN_ALTITUDE);
+        var marker_drawable = new AR.ImageDrawable(marker_image, 8);
+        var marker_object = new AR.GeoObject(marker_loc, {
+            drawables: {
+              cam: [marker_drawable]
+            }
+        });
+
+        World.worldLoaded();
+
+        AR.logger.debug("Added marker");
+    },
+
+    worldLoaded: function worldLoadedFn() {
+        World.loaded = true;
     }
-  });
-
-  AR.logger.debug("lat:"+lat+", long: "+lon);
-
 }
 
-AR.logger.activateDebugMode();
+World.init();
 
-AR.context.onLocationChanged = locationChanged;
+// liten on change and run this callback World.createModelAtLocation
+
