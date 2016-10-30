e6acb7fd7b6fd970674e083b6485a6d008f5e071
Nick Breaton <nick@breaton.com>
2016-10-30 03:57:56 -0400

    Add map to React

--- FriendFinderAndroid/app/src/main/assets/finder/src/components/Map.js
+++ FriendFinderAndroid/app/src/main/assets/finder/src/components/Map.js
@@ -1,15 +1,123 @@
 import React from 'react';
 
-export default (props) => (
-  <iframe
-    style={{opacity: 0.8}}
-    width={props.width}
-    height={window.innerHeight + 50}
-    frameBorder="0"
-    scrolling="no"
-    marginHeight="0"
-    marginWidth="0"
-    title="Example"
-    src="//nickbreaton.maps.arcgis.com/apps/Embed/index.html?webmap=754383b8c86a42b6b64dbe0156f1f7bd&amp;extent=-122.2038,25.7531,-68.239,48.7436&amp;zoom=true&amp;scale=true&amp;disable_scroll=true&amp;theme=light"
-  />
-)
+export default class Map extends React.Component {
+  componentDidMount() {
+    window.require([
+        "esri/core/urlUtils",
+        "esri/Map",
+        "esri/views/SceneView",
+        "esri/layers/FeatureLayer",
+        "esri/renderers/UniqueValueRenderer",
+        "esri/symbols/ExtrudeSymbol3DLayer",
+        "esri/symbols/PolygonSymbol3D",
+        "esri/symbols/SimpleFillSymbol",
+        "esri/widgets/Track",
+        //"esri/dijit/Directions",
+        "dojo/parser",
+        "dijit/layout/BorderContainer",
+        "dijit/layout/ContentPane",
+        "dojo/domReady!"
+    ], function(
+      urlUtils,
+      Map,
+      SceneView,
+      FeatureLayer,
+      UniqueValueRenderer,
+      ExtrudeSymbol3DLayer,
+      PolygonSymbol3D,
+      SimpleFillSymbol,
+      Track,
+      parser
+    ) {
+        parser.parse();
+        //all requests to route.arcgis.com will proxy to the proxyUrl defined in this object.
+        urlUtils.addProxyRule({urlPrefix: "route.arcgis.com", proxyUrl: "/sproxy/"});
+        urlUtils.addProxyRule({urlPrefix: "traffic.arcgis.com", proxyUrl: "/sproxy/"});
+
+        var map = new Map({basemap: "dark-gray"});
+
+        var size = 100
+        var view = new SceneView({
+            container: "viewDiv",
+            map: map,
+            zoom: 17,
+            center: [-79.0458, 35.9095]
+        });
+        /********************
+             * Add feature layer
+             ********************/
+
+        var symbol = new PolygonSymbol3D({
+            symbolLayers: [new ExtrudeSymbol3DLayer({
+                    size: size, // 100,000 meters in height
+                    //material: { color: size > 300 ? 'red' : 'blue' }
+                })]
+        });
+        var template = {
+            "fieldInfos": [
+                {
+                    "fieldName": "Address",
+                    "format": {
+                        "places": 0,
+                        "digitSeparator": true
+                    }
+                }
+            ],
+            "title": "Found! {Address} amount of friends here"
+        };
+        //var citiesRenderer = new UniqueValueRenderer({
+        //symbol: symbol
+        //});
+        var renderer = new UniqueValueRenderer({field: "Address", defaultSymbol: new SimpleFillSymbol()});
+        renderer.addUniqueValueInfo("high", new SimpleFillSymbol({color: "red"}));
+        renderer.addUniqueValueInfo("mid", new SimpleFillSymbol({color: "green"}));
+        renderer.addUniqueValueInfo("low", new SimpleFillSymbol({color: "yellow"}));
+        renderer.visualVariables = [
+            {
+                type: "opacityInfo",
+                field: "Address",
+                normalizationField: "SQ_KM",
+                // features with 30 ppl/sq km or below are assiged the first opacity value
+                stops: [
+                    {
+                        value: 100,
+                        opacity: 0.15
+                    }, {
+                        value: 1000,
+                        opacity: 0.90
+                    }
+                ]
+            }
+        ];
+        // Carbon storage of trees in Warren Wilson College.
+        var featureLayer = new FeatureLayer({
+          url: "http://services7.arcgis.com/cS890GsOFd26sODz/arcgis/rest/services/UNCV2/FeatureServer/0",
+          renderer: renderer,
+          refreshInterval: 0.01
+        });
+
+        var track = new Track({view: view});
+        view.ui.add(track, "top-left");
+        // The sample will start tracking your location
+        // once the view becomes ready
+        view.then(function() {
+            // track.start();
+        });
+
+        featureLayer.popupTemplate = template;
+        map.add(featureLayer);
+
+        setInterval(() => {
+          var renderer = new UniqueValueRenderer({field: "Address", defaultSymbol: new SimpleFillSymbol()});
+          renderer.addUniqueValueInfo("high", new SimpleFillSymbol({color: "red"}));
+          renderer.addUniqueValueInfo("mid", new SimpleFillSymbol({color: "green"}));
+          renderer.addUniqueValueInfo("low", new SimpleFillSymbol({color: "yellow"}));
+          featureLayer.renderer = featureLayer.renderer.clone();
+        }, 1000);
+    });
+  }
+
+  render() {
+    return <div id="viewDiv" style={{opacity: 0.85}}/>
+  }
+}
